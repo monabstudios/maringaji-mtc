a18941e9cdd5673b100405a2de40ff4ad631e084
Mohamed Afiq <monabstudios@gmail.com>
2018-05-22 22:56:28 +0800

    Added login API. Added permissions and authentication to profiles API.

--- maringaji/maringaji_api/views.py
+++ maringaji/maringaji_api/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
+from . import permissions
 from . import serializers
 from . import models
 
@@ -12,5 +16,18 @@ from . import models
 class UserProfileViewSet(viewsets.ModelViewSet):
   """Handles creating, reading and updating User Profiles."""
 
+  print("hello")
+
   serializer_class = serializers.UserProfileSerializer
   queryset = models.UserProfile.objects.all()
+  authentication_classes = (TokenAuthentication,)
+  permission_classes = (permissions.UpdateOwnProfile,)
+
+
+class LoginViewSet(viewsets.ViewSet):
+  """Handles logging in."""
+
+  serializer_class = AuthTokenSerializer
+
+  def create(self, request):
+    return ObtainAuthToken().post(request)
